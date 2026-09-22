1a57fa14e9863cfaf8486116bb6e8321c3058fd9
Cauã Gomes <devcauagomes@gmail.com>
2026-09-22 09:15:02 -0300

    fix: atualiza fallback do TypeORM para usar pooler do Supabase na porta 6543

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,14 @@ import { Sensor } from './sensors/entities/sensor.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL || 'postgresql://postgres:aqulesdevs2@db.jluuhlexkpdziuoiaswv.supabase.co:5432/postgres',
+      url: process.env.DATABASE_URL || 'postgresql://postgres.jluuhlexkpdziuoiaswv:aqulesdevs2@aws-0-br-south-1.pooler.supabase.com:6543/postgres',
       entities: [Sala, Sensor],
       synchronize: true,
       ssl: {
         rejectUnauthorized: false,
       },
       extra: {
-        family: 4, // Força o uso de IPv4, evitando o erro ENETUNREACH
+        family: 4,
       },
     }),
     SalasModule,
